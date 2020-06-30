689495a30bf05335722a5a4e44c0df450ce6c865
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-06-30 12:00:29 +0530

    create without error

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('CRUD app is running!');
+    expect(page.getTitleText()).toEqual('crud app is running!');
   });
 
   afterEach(async () => {
