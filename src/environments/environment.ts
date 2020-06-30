ad51468ecad0e2f6087b6317b20d796febcd4a38
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-07-01 00:00:45 +0530

    database conected boostrp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false , 
+  firebaseConfig: {
+    apiKey: "AIzaSyC8i8Ugl9EN5U2XD7LzLCQm5ytLH75cQNc",
+    authDomain: "angularfirestorecrud-14c04.firebaseapp.com",
+    databaseURL: "https://angularfirestorecrud-14c04.firebaseio.com",
+    projectId: "angularfirestorecrud-14c04",
+    storageBucket: "angularfirestorecrud-14c04.appspot.com",
+    messagingSenderId: "400392764830",
+    appId: "1:400392764830:web:9530e3e68516ceed5d6a8f",
+    measurementId: "G-EZCFF7CWC4"
+  }
 };
 
 /*
